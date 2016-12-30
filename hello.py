114954171c55a62ff5546ece69b996d95ed19ac7
chiyx <chiyxme@outlook.com>
2016-12-31 02:16:46 +0800

    ch05 learn

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-def spam(divideBy):
-    return 42 / divideBy
+spam = {'color': 'red', 'age': 42}
 
-try:
-    print(spam(2))
-    print(spam(0))
-except ZeroDivisionError as error:
-    print('Error: Invalid argument: ' + str(error))
+for v in spam.values():
+    print(v)
