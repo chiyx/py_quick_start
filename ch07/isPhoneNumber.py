c8b2beb9d7666d9b3dd3fa23524040ac93dfc789
chiyx <chiyxme@outlook.com>
2017-02-07 00:04:33 +0800

    ch07 phoneAndEmail

--- ch07/isPhoneNumber.py
+++ ch07/isPhoneNumber.py
@@ -4,4 +4,15 @@ import re
 
 phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 mo = phoneNumRegex.search('My number is 415-555-4242')
-print(mo.group(0))
+
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+
+# 贪心匹配 and 非贪心匹配
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHaHa')
+print(mo1.group())
+
+greedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo1 = greedyHaRegex.search('HaHaHaHaHaHa')
+print(mo1.group())
